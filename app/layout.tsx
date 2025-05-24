b1d0917711a58dfb277dfce6acd5317ff396587c
Arin7712 <aringawande7712@gmail.com>
2025-05-24 14:29:47 +0530

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import "./globals.css";
 
-const font = Poppins({ weight: ["400", "500", "600", "700", "800", "900"] });
+const font = Poppins({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Scooter - Sales hiring, solved",
